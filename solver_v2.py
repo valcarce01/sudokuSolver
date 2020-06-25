4464c214df2d5bc84bce985c39280ecba89da2b1
Diego Valcarce <dvalcarcer@gmail.com>
2020-06-25 23:27:38 +0200

    Possibilities marker working

--- solver_v2.py
+++ solver_v2.py
@@ -3,30 +3,38 @@ Tries to solve a sudoku
 author: @Diego Valcarce
 """
 import sudoku_v2
-
+import time
 class solver:
     def __init__(self, sudoku):
         """
         + sudoku: must be a sudoku_v2 object
         """
         self.sudoku = sudoku
-        self.generate_posibilities()
+        added_numbers = self.generate_posibilities()
+        
+        a = time.time()
+        while self.generate_posibilities() > added_numbers:
+            added_numbers = self.generate_posibilities()
+            print(added_numbers)
+        print("All we can do working with columns and rows done in: ", time.time() - a)
+        # for item in self.sudoku._sudoku_values.items():
+        #     print(item)
 
     def generate_posibilities(self):
         """
         Creates all the posibilities
         """
+        # We'll create a counter to know how long we can handle to be executing the function (as long as the cnt increases)
+        added_numbers = 0
+
         possibilities = set(range(1, 10))
         for row_index in range(9):
             # Let's see what numbers we got
-            row = set(self.sudoku.get_row(row_index))
             row = set([item for item in self.sudoku.get_row(row_index) if not isinstance(item, list)])
             for col_index in range(9):
                 # We just need to get the value if we want to mark it (AKA we don't actually know the rial value)
-                if self.sudoku.get_value((col_index, row_index)) == 0:
+                if self.sudoku.get_value((col_index, row_index)) == 0 or isinstance(self.sudoku.get_value((col_index, row_index)), list):
                     # The column we are studying:
-                    # col = set(self.sudoku.get_column(col_index))
-                    # print(self.sudoku.get_column(col_index))
                     col = set([item for item in self.sudoku.get_column(col_index) if not isinstance(item, list)])
                     # On each position we can use the values in `posibilities` that are not in
                     # the union of what we got on col + row
@@ -44,11 +52,13 @@ class solver:
                     # that will mean that in that position, that value is the only one that can be setted
                     if len(pos_list) == 1:
                         self.sudoku.add_value(pos_list[0], (col_index, row_index))
+                        added_numbers += 1
+
                     else:
                         self.sudoku.add_marker(pos_list, (col_index, row_index))
+        return added_numbers
         
-        for item in self.sudoku._sudoku_values:
-            print(item)
+        
 
     def _get_block_(self, index: tuple):
         """
