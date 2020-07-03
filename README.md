fc1ba69a59297d5d2c8948ff1eac021088f96ea5
Diego Valcarce <dvalcarcer@gmail.com>
2020-07-03 16:12:39 +0200

    Project ended

--- README.md
+++ README.md
@@ -70,6 +70,14 @@ The first done is to asign a name to each cell, I decided to like:
 </tbody>
 </table>
 
-After that, it's created a graph (for after solving it), the graph has as vertices all the cells and, the edges, connect 2 vertices that can not have the same value. For example, in the table above, A1 can not have a value that is in A2, A3, A4, B1, C1, D1 or A4, as the rules for the sudoku does not allow to have the same values in rows, columns or squares.
+After that, it's created a graph (for after solving it), the graph has as vertices all the cells and, the edges, connect 2 vertices that can not have the same value. For example, in the table above, A1 can not have a value that is in A2, A3, A4, B1, C1, D1 or A4, as the rules for the sudoku does not allow to have the same values in rows, columns or squares. The graph seems to be something like this:
 
 ![alt text](https://github.com/valcarce01/sudokuSolver/blob/master/files/graph.PNG "Graph example of a 4x4 sudoku")
+
+Each vertex will store its name and a set of values (the ones which it can have). For the vertices we know (given on the table before starting the game), we travel to all the vertices it's pointing to and do remove that posibility. 
+
+The 'algorithm' just does what has been explained in loop (while removing numbers) and, when not adding, checks wheter it has already won; if not, bad luck, it's not planned to be able to try out the many (or not) possibilities that can actually handle.
+
+Although it can not solve all sudokus, the ones it can, are solved in an average of less than 0.001 seconds, due to the fact of the graph implementation and working with numpy arrays (as matrix for the sudoku indexes values).
+
+The code is provided of a pair of examples: a low-level one and a mid-level one.
--- sudoku.py
+++ sudoku.py
@@ -1,110 +1,175 @@
-""""
-by: @Diego Valcarce
-
-class sudoku:
-    + add a number
-    + mark as possible answer
-"""
+#   ***     IMPORTATIONS    ***
 import numpy as np
 import time
-##############
-# Exceptions:
-class AlreadyOnTable(Exception):
-    """
-    Raised when trying to set a value that it´s already set.
-    """
-    pass
-
-##############
+import copy
 
 class sudoku:
     """
-    Will be stored as a pair of a matrix implemented in numpy and a dictionary (hash table) wich actually has
-    the values.
+    Own sudoku structure created by @Diego Valcarce to be able to analyze properly the values and get the solution. Once it's created
+    the sudoku object, will be created automatically 3 objects:
+
+    + numpy array: will store the indexes of the sudoku (A1, A2 ..., B1, B2... like), this will be used to access easily (and qucikly)
+        to all the indexes of the sudoku.
+    + hash table (dict like): will store the real values of the sudoku, easy to access from the numpy array
+    + graph: a non-directed simple implemented graph, used to travel around the posible solutions and implementate a backtracking algorithm
+        to solve it
     """
-    def __init__(self, values, unknown = 0):
+    
+    # ***       NON-DIRECTED GRAPH      ***
+    class Graph:
         """
-        + values: set of values of the sudoku, all in the same input.
-        + unknown: parameter that identifies the unknown values.
+        Non directed graph implementation
         """
+        def __init__(self):
+            """
+            Creates an empty graph. Data will be store as a 'JSON' (will be a dict of dicts):
+            data = {
+                "vertex": {
+                    POS_VALUES = set(),         # Can't be modified, up until be sure a value can be dropped
+                    degree = int,
+                    try: int                    # The version of the data we are trying to check as a posible solution
+                }
+                ...
+            }
+            """
+            self._data = dict()
+        
+        def add_vertex(self, vertex, POS_VALUES = set(range(1, 10))):
+            """
+            Adds a vertex
+            """
+            self._data[vertex] = {
+                "POS_VALUES" : POS_VALUES,         # By default can have this 9 values
+                "degree" : 0,
+                "try" : None,
+                "point_to": set()
+            }
+
+        def add_edge(self, vertex_from, vertex_end):
+            """
+            Adds an edge between 2 existing vertices
+            """
+            # When creating an edge we might:
+            #   1.- Adding the edge
+            #   2.- Increasing the degree
+            self._data[vertex_from]["point_to"].add(vertex_end)
+            self._data[vertex_from]["degree"] += 1
+
+            self._data[vertex_end]["point_to"].add(vertex_from)
+            self._data[vertex_end]["degree"] += 1
+        
+        def degree(self, vertex):
+            """
+            Returns the degree of a given vertex
+            """
+            return self._data[vertex]["degree"]
+
+        def remove_non_accepted_values(self):
+            """
+            Removes from the graph the values that can not be taken.
+            """
+            added_numbers = 0
+            def _remove_():
+                added_numbers = 0
+                for vertex in self._data.keys():
+                    val = self._data[vertex]["POS_VALUES"]
+                    if len(val) == 1:
+                        # In case we now a number can not be in all the other positions, we remove it
+                        for vertex_pointed in self._data[vertex]["point_to"]:
+                            difference = self._data[vertex_pointed]["POS_VALUES"] 
+                            self._data[vertex_pointed]["POS_VALUES"] = difference.difference(val)
+                            added_numbers += len(difference) - len(self._data[vertex_pointed]["POS_VALUES"])
+                return added_numbers
+            
+            while (added_numbers + _remove_()) > added_numbers:
+                added_numbers = _remove_()
+            return {vertex: self._data[vertex]["POS_VALUES"] for vertex in self._data.keys()}
+
+
+        def __str__(self):
+            """
+            Prints the edges of the graph
+            """
+            string = ""
+            for vertex in self._data.keys():
+                pointing = self._data[vertex]["point_to"]
+                values = self._data[vertex]["POS_VALUES"]
+                string += f"{vertex} points to {pointing} and can have the {values} as values \n"
+            return string
+
+
+
+    def __init__(self, sudoku, unknown = 0):
+        """
+        + sudoku: a sudoku like; must be an array of the values that has the own sudoku. Ordered by row/col
+        + unknown: the parameter inclyed in the sudoku that has the meaning of unknown.
+        """
+        start_time_graph = time.time()
         self._unknown_parameter = unknown
         # Create the table wi will play with:
         self._sudoku_table = np.array([[chr(letter) + str(number) for number in range(1, 10)] for letter in range(65, 74)])
         # Create the dictionary that will store the rial data:
-        self._sudoku_values = dict(zip([chr(letter) + str(number) for letter in range(65, 74) for number in range(1, 10)], values))
+        self._sudoku_values = dict(zip([chr(letter) + str(number) for letter in range(65, 74) for number in range(1, 10)], sudoku))
 
-        print(self._sudoku_table, "\n\n", self._sudoku_values)
-
-    def add_value(self, value, position):
-        """
-        Add a value in a certain position of the sudoku.
-        + Position must be a tuple like: (5, 2), where 5 is the column and 2 the row.
-        """
-        pos = self._sudoku_table[position[1]][position[0]]
-        # Check wheter it's an actual value or not
-        if (isinstance(self._sudoku_values[pos], list) or self._sudoku_values[pos] == self._unknown_parameter):
-            self._sudoku_values[pos] = value
-        else:
-            raise AlreadyOnTable("There's already a value on that position")
-
-
-    def add_marker(self, mark, position):
-        """
-        Adds a mark of possible answers on a certain positions of the sudoku
-        + Position must be a tuple like: (5, 2), where 5 is the column and 2 the row.
-        """
-        self._sudoku_values[self._sudoku_table[position[1]][position[0]]] = [*mark] if (isinstance(mark, tuple) or isinstance(mark, list)) else [mark]
-
-    def get_value(self, position):
-        """
-        Return the value of a position
-        """
-        return self._sudoku_values[self._sudoku_table[position[1]][position[0]]]
-    
-    def get_value_by_name(self, name):
-        """
-        Returns the value of a given position, like A1, A2...
-        """
-        return self._sudoku_values[name]
-
-    def get_row(self, index):
-        """
-        Returns the whole row given an index
-        """
-        return [self._sudoku_values[n] for n in self._sudoku_table[index]]
-
-    def get_column(self, index):
-        """
-        Returns a whole column
-        """
-        return [self._sudoku_values[n] for n in self._sudoku_table[:,index]]
-
-    def get_block(self, block):
-        """
-        Returns block. Distributed like:
-        ____________________
-         0  |   1   |   2
-         ___|_______|_______
-            |       |
-         3  |   4   |    ....
+        # Now create the graph:
+        self.graph = self.Graph()
         
-        will return the values in order, like:
-        block 1: A1 A2 A3 B1 B2 B3 C1 C2 C3...
+        # Create the vertices:
+        self.vertices = [chr(letter) + str(number) for letter in range(65, 74) for number in range(1, 10)]
+        # And add them
+        for vertex in self.vertices:
+            if self._sudoku_values[vertex] != unknown:
+                self.graph.add_vertex(vertex, {self._sudoku_values[vertex]})
+            else:
+                self.graph.add_vertex(vertex)
+
+        #       ***     CREATING EDGES      ***
+        table = self._sudoku_table
+        # In the first step we add cols and rows
+        for index in range(9):
+            row = table[index]
+            col = table[:, index]
+            combinations_row = [(a, b) for a in row for b in row if a != b and b != a]
+            combinations_col = [(a, b) for a in col for b in col if a != b and b != a]
+            # And create the combinations as edges on the graph
+            for edge_index in range(len(combinations_row)):
+                self.graph.add_edge(*combinations_row[edge_index])
+                self.graph.add_edge(*combinations_col[edge_index])
+        # Finally the blocks
+        block = self.get_block_combinations()
+        for block_values in block:
+            combinations = [(a, b) for a in block_values for b in block_values if a != b and b != a]
+            for edge in combinations:
+                self.graph.add_edge(*edge)
+        #       ********************************
+        end_time_graph = time.time()
+        print("Data structure for organazing the sudoku created in {} seconds".format(end_time_graph - start_time_graph))
+
+        # Once it has been created the vertices and the edges, it's time to remove the values that can not take place
+        start_time_removing_non_valid = time.time()
+        self._sudoku_values = self.graph.remove_non_accepted_values()
+        end_time_removing_non_valid = time.time()
+        print("Remove all the invalid values toke {} seconds".format(end_time_removing_non_valid - start_time_removing_non_valid))
+        
+
+    def solve(self):
         """
-        # The up/down will be deffined by entire division (//)
-        # The right/left by the module (%)
-        up = (block) // 3 * 3
-        left = (block) % 3 * 3
+        Solves the sudoku (tries to)
+        """ 
+        # We need to figure out wheter we already won:
+        if self.check_win():
+            print("Sudoku solved")
+            solutions = ""
+            for index, value in self._sudoku_values.items():
+                solutions += f"{index} -> {value} \n"
+            return solutions
+        
+        else:
+            return "Not possible to solve with this program"
+        
 
-        # Now create the return
-        # First step is to get the indexes for the dictionary
-        return_list = []
-        for row in self._sudoku_table[up : up + 3, left : left + 3]:
-            for index in row:
-                return_list.append(self._sudoku_values[index])
+        
 
-        return return_list
-    
     def get_block_combinations(self):
         """
         Returns a list of combinations of the letters (A1, A2...) for each block
@@ -123,62 +188,28 @@ class sudoku:
 
         return return_list
 
-
-    def get_block_index(self, block):
-        """
-        Returns a list of the indexes of the actual block
-        """
-        up = (block) // 3 * 3
-        left = (block) % 3 * 3
-        return_list = []
-        for row in self._sudoku_table[up : up + 3, left : left + 3]:
-            for index in row:
-                return_list.append(index)
-
-        return return_list
-
     def check_win(self):
         """
-        Check if it's solved
+        Checks if win
         """
-        # We need to call the _check_win_unique and after that, check the boxes
-        result = []
-        for index in range(9):
-            result.append(self._check_win_unique_(self.get_column(index)))
-            result.append(self._check_win_unique_(self.get_row(index)))
-            result.append(self._check_win_unique_(self.get_block(index)))
-        set_result = set(result)
-        # If all are OKs, the len should be 1, 'cause it will be all True:
-        if len(set_result) != 1:
-            return False
-        
-        # So, if it's of len 1, we need to check if it's a False or True
-        for element in iter(set_result):
-            if element:
-                return True
-            else:
-                return False
-        
+        check_graph = [self.graph._data[v]["POS_VALUES"] for v in self.graph._data.keys()]
+        result = True
+        for i in check_graph:
+            result = result and (len(i) == 1)
+        return result
 
-    def _check_win_unique_(self, to_check):
-        """
-        Given a list of values, checks if are all different in the same list. So can be an actual solution.
-        """
-        try:  
-            return len(set(to_check)) == 9
-        except TypeError:       # If a list
-            return False
 
-def example_sudoku():
-    return [5, 3, 0, 0, 7, 0, 0, 0, 0,
-        6, 0, 0, 1, 9, 5, 0, 0, 0,
-        0, 9, 8, 0, 0, 0, 0, 6, 0,
-        8, 0, 0, 0, 6, 0, 0, 0, 3,
-        4, 0, 0, 8, 0, 3, 0, 0, 1,
-        7, 0, 0, 0, 2, 0, 0, 0, 6,
-        0, 6, 0, 0, 0, 0, 2, 8, 0,
-        0, 0, 0, 4, 1, 9, 0, 0, 5,
-        0, 0, 0, 0, 8, 0, 0, 7, 9]
+
+    def __str__(self):
+        return self.graph.__str__()
+        
+    def _sort_by_len_values(self, grpah_to_sort):
+        """
+        Returns the sorted dictionary by the len of the values
+        """
+        return dict(sorted(grpah_to_sort._data.items(), key = lambda x: x[1]["len"]))
+
+
 
 if __name__ == "__main__":
     s = sudoku(
@@ -192,4 +223,19 @@ if __name__ == "__main__":
         0, 0, 0, 4, 1, 9, 0, 0, 5,
         0, 0, 0, 0, 8, 0, 0, 7, 9]
     )
-    
+    print(s.solve())
+
+    sudo = sudoku(
+        [
+            0, 0, 1, 3, 7, 8, 0, 0, 9,
+            3, 2, 0, 9, 0, 0, 0, 5, 0,
+            9, 7, 4, 1, 0, 2, 6, 3, 0,
+            0, 1, 6, 0, 8, 0, 0, 0, 0,
+            7, 9, 0, 0, 0, 0, 0, 8, 5,
+            0, 0, 0, 0, 3, 0, 1, 9, 0,
+            0, 6, 9, 4, 0, 3, 8, 7, 2,
+            8, 4, 0, 0, 0, 7, 5, 1, 3,
+            1, 0, 0, 8, 2, 5, 9, 0, 0
+        ]
+    )
+    print(sudo.solve())
